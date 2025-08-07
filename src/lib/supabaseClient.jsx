14f2fece0fcdcfbcfd353da0df92aeca15f51b18
JeffreyG244 <JeffreyG244@users.noreply.github.com>
2025-08-07 14:30:34 -0500

    Add supabaseClient.jsx for production build

---
+++ src/lib/supabaseClient.jsx
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://tzskjzkolyiwhijslqmq.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR6c2tqemtvbHlpd2hpanNscW1xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg2NTY3ODAsImV4cCI6MjA2NDIzMjc4MH0.EvlZrWKZVsUks6VArpizk98kmOc8nVS7vvjUbd4ThMw'
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
+export default supabase
